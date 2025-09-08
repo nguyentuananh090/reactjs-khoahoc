213afde2da08fc6b3a16005ac3f3eb5ffe405e7e
nguyentuananh090 <nguyentuananh02091992@gmail.com>
2025-09-08 11:07:07 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,12 @@
+//fragment
+import "./style.css"
 const MyComponent = () => {
     return (
+        <>
         <h1>Hello word 2</h1>
+        <h1 className="test_fragment" style={{border:"1px solid"}}>Hello word test</h1>
+        </>
+        
     );
 }
 export default MyComponent;
