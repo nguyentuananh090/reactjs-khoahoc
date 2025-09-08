5e62095f20af40e1c429370109e4553648acc297
nguyentuananh090 <nguyentuananh02091992@gmail.com>
2025-09-08 14:47:46 +0700

    video 36

--- src/components/baitap/AddComponent.jsx
+++ src/components/baitap/AddComponent.jsx
@@ -1,11 +1,21 @@
 const AddData=(props)=>{
     const {addNewFunc} = props;
-    addNewFunc("TA")
+    // addNewFunc("TA");
+    let dem=1;
+    const handleClick=()=>{
+        alert("Click handleClick "+(dem++));
+    }
+    // const handleOnChange=(event)=>{
+    //     console.log("Click handleOnChange",event.target.value); 
+    // }
+    const handleOnChange=(name)=>{
+        console.log("Click handleOnChange",name); 
+    }
     return (
         <>
         <div className="form-todo">
-          <input type="text" name="name" />
-          <input type="submit" value="Submit" />
+          <input onChange={(event) =>handleOnChange(event.target.value)} type="text" name="name" />
+          <input onClick={handleClick} style={{cursor:'pointer'}} type="submit" value="Submit" />
         </div>
         </>
     )
